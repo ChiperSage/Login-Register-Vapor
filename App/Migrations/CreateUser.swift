f463b8511f5a7d75f8b4ced1583aaa1910bfc88b
ByteNinja <pijayblog@gmail.com>
2024-08-08 10:05:30 +0700

    Update CreateUser.swift

--- App/Migrations/CreateUser.swift
+++ App/Migrations/CreateUser.swift
@@ -1,1 +1,21 @@
-.
+import Fluent
+
+struct CreateUser: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("users")
+            .field("user_id", .int, .identifier(auto: true))
+            .field("username", .string, .required)
+            .field("password", .string, .required)
+            .field("login_attempts", .int, .required, .default(0))
+            .field("last_login_attempt", .datetime)
+            .field("remember_me_token", .string)
+            .field("created_at", .datetime, .required, .default(.now))
+            .field("updated_at", .datetime, .required, .default(.now), .onUpdate(.now))
+            .unique(on: "username")
+            .create()
+    }
+
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("users").delete()
+    }
+}
